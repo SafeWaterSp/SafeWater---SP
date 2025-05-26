62198e5bf1da37c7294a98289b6d22a7a0bc3e73
Pedro Almeida <pedrominato23@gmail.com>
2025-05-26 15:40:13 -0300

    Criando estrutura basica do css

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,18 @@
-console.log("Funcionando!")
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      }
+    });
+  },
+  {
+    threshold: 0.1,
+  }
+);
+
+sections.forEach((section) => {
+  observer.observe(section);
+});
