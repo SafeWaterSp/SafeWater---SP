b3a4ddc987dac58fa7142dad10747eafaa3e5e8b
Luiz <nandoluizprimeiro@gmail.com>
2025-05-28 10:32:58 -0300

    Deixando o cabeçalho muito foda. Transitions e transparente

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,8 @@
-const sections = document.querySelectorAll("section");
-
-const observer = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("visible");
-      }
-    });
-  },
-  {
-    threshold: 0.1,
+window.addEventListener("scroll", function () {
+  let header = document.querySelector("header");
+  if (window.scrollY > 10) { // Se rolou mais de 10px
+    header.style.backgroundColor = "rgb(9, 9, 85)"; // Azul sólido
+  } else {
+    header.style.backgroundColor = "rgba(0, 0, 128, 0)"; // Transparente
   }
-);
-
-sections.forEach((section) => {
-  observer.observe(section);
 });
